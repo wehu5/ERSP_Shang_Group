df44727cb3268ea2d02cee2619dab15c45275ec0
mhsiu01 <78574718+mhsiu01@users.noreply.github.com>
2022-05-15 04:33:13 -0700

    Fixed doc_to_class
    Now doc_to_class only maps low-confidence documents to their class. Previously it contained class assignment for all documents when it should only have assignment for low-confidence documents.

--- scripts/cluster_utils.py
+++ scripts/cluster_utils.py
@@ -17,9 +17,11 @@ from tqdm import tqdm
 from operator import itemgetter
 from collections import defaultdict
 
-def generate_keywords(tokenization_info, doc_to_class, num_clusters):
+def generate_keywords(tokenization_info, all_docs_to_class, doc_indices, num_clusters):
     
     docs = [x[0] for x in tokenization_info]
+    # doc_to_class for only the low confidence documents
+    doc_to_class = [ all_docs_to_class[i] for i in doc_indices ]
 
     # num_clusters = len(classes)
     # Sanity-check
